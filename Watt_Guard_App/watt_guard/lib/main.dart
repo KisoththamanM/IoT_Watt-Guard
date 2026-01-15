d6fcfb0041460839fdb5deebca706d58a92c01f8
KisoththamanM <mkisoththamanofficial@gmail.com>
2026-01-15 16:40:10 +0530

    UI improvements: Display power, Equipment status (May be some emojis), Power switch for the equipment.

--- Watt_Guard_App/watt_guard/lib/main.dart
+++ Watt_Guard_App/watt_guard/lib/main.dart
@@ -26,36 +26,93 @@ class _MyAppState extends State<MyApp> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'WATT Guard',
-                  style: TextStyle(
-                    color: Colors.orangeAccent,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 30,
-                    height: 2,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  width: screenWidth / 2 - 15,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(color: Colors.white60, width: 1),
-                  ),
-                  child: Center(
-                    child: Text(
-                      '3.5 A',
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'WATT Guard',
                       style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 50,
-                        height: 1,
+                        color: Colors.orangeAccent,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30,
+                        height: 2,
                       ),
                     ),
-                  ),
+                    Icon(
+                      Icons.electric_bolt_rounded,
+                      color: Colors.yellow,
+                      size: 35,
+                    ),
+                  ],
                 ),
-
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          width: screenWidth / 2 - 15,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(color: Colors.white60, width: 1),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '3.5 A',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50,
+                                height: 1,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          width: screenWidth / 2 - 15,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(color: Colors.white60, width: 1),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '15 W',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50,
+                                height: 1,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: screenWidth / 2 - 15,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Status',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 50,
+                            height: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
                 SizedBox(height: 10.0),
                 Container(
                   padding: EdgeInsets.all(10),
@@ -80,6 +137,10 @@ class _MyAppState extends State<MyApp> {
                   ),
                 ),
                 SizedBox(height: 10.0),
+                Text(
+                  'Safety status and Power switch',
+                  style: TextStyle(color: Colors.white, height: 2),
+                ),
                 Container(
                   padding: EdgeInsets.all(10),
                   width: screenWidth - 20,
@@ -98,6 +159,16 @@ class _MyAppState extends State<MyApp> {
                           fontSize: 50,
                           height: 1,
                         ),
+                      ),
+                      Switch(
+                        value: isSwitchOn,
+                        onChanged: (value) {
+                          setState(() {
+                            isSwitchOn = value;
+                          });
+                        },
+                        activeThumbColor: Colors.black,
+                        inactiveThumbColor: Colors.black87,
                       ),
                     ],
                   ),
