1ab1d7568c977851823fc30f37198e9444fda457
KisoththamanM <mkisoththamanofficial@gmail.com>
2026-01-25 12:46:27 +0530

    Current sensor reading has been got and sent to MQTT broker.

--- Arduino_code/Watt_Guard/Watt_Guard.ino
+++ Arduino_code/Watt_Guard/Watt_Guard.ino
@@ -1,52 +1,54 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "ACS712.h"
 
 #define RELAY_PIN 26
+#define CURRENT_PIN 32
+ACS712 currentSensor(CURRENT_PIN, 5.0, 4095, 185);
 
-const char* ssid = "Wifi";
+const char* ssid = "WiFi";
 const char* password = "********";
 const char* mqtt_server = "broker.hivemq.com";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setupWiFi() {
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+//For millis()
+unsigned long lastSendTime = 0;
+const unsigned long interval = 1000;
+
+void setupWiFi(){
+  WiFi.begin(ssid,password);
+  while (WiFi.status()!= WL_CONNECTED){delay(500);}
+  Serial.println("Wifi connected..........");
 }
 
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
+void mqttCallback(char* topic, byte* payload, unsigned int length){
   String message = "";
-
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++){
     message += (char)payload[i];
   }
-
-  if (String(topic) == "iot/wattguard/relay") {
-    if (message == "ON") {
-      digitalWrite(RELAY_PIN, LOW);   // Relay ON
+  if(String(topic) == "WattGuard/relay"){
+    if(message == "ON"){
+      digitalWrite(RELAY_PIN, LOW);
     }
-    else if (message == "OFF") {
-      digitalWrite(RELAY_PIN, HIGH);  // Relay OFF
-    }
-  }
-}
-
-void reconnectMQTT() {
-  while (!client.connected()) {
-    if (client.connect("esp32_wattguard")) {
-      client.subscribe("iot/wattguard/relay");
-    } else {
-      delay(2000);
+    else if (message == "OFF"){
+      digitalWrite(RELAY_PIN, HIGH);
     }
   }
 }
 
-void setup() {
+void connectMQTT(){
+  while(!client.connected()){
+    if(client.connect("ESP32_WattGuard")){
+      client.subscribe("WattGuard/relay");
+    }
+    else{delay(2000);}
+  }
+}
+
+void setup(){
   Serial.begin(9600);
-
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH); // Relay OFF initially
 
@@ -54,11 +56,33 @@ void setup() {
 
   client.setServer(mqtt_server, 1883);
   client.setCallback(mqttCallback);
+
+  //ACS712 Calibration
+  Serial.println("Calibrating ACS712 at no load");
+  delay(2000);
+  currentSensor.autoMidPoint();
+  Serial.println("Calibration done!");
 }
 
 void loop() {
   if (!client.connected()) {
-    reconnectMQTT();
+    connectMQTT();
   }
   client.loop();
+
+  unsigned long now = millis();
+  if(now - lastSendTime >= interval){
+    lastSendTime = now;
+    float current = currentSensor.mA_AC()/1000.0;
+    
+    //Checking serial print
+    Serial.print("Current: ");
+    Serial.print(current, 3);
+    Serial.println(" A");
+
+    //Publish to MQTT
+    char payload[10];
+    dtostrf(current, 4, 3, payload);
+    client.publish("WattGuard/current", payload);
+  }
 }
